4f9317e4caf18c1697dfbc0f5e6faa3ba1560f10
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-09-11 11:35:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 peter_pan_song="";
 harry_potter_song="";
+leftWristX= 0;
+leftWristY= 0;
+rightWristX= 0;
+rightWristY= 0;
 
 function preload(){
     peter_pan_song=loadSound('music.mp3');
@@ -11,7 +15,26 @@ function setup(){
     canvas.center();
     video= createCapture(VIDEO);   
     video.hide();     
+    posenet=ml5.poseNet(video,modelLoaded);     
+    posenet.on('pose', gotPoses); 
 }
+
+function modelLoaded(){     
+console.log('Model Loaded!');
+}
+
+function gotPoses(results){     
+    if(results.length > 0){         
+    console.log(results);    
+    leftWristX= results[0].pose.leftWrist.x;     
+    leftWristY= results[0].pose.leftWrist.y;     
+    console.log('leftWristX = '+leftWristX+' leftWristY = '+leftWristY);  
+    rightWristX= results[0].pose.rightWrist.x;     
+    rightWristY= results[0].pose.rightWrist.y;     
+    console.log('rightWristX = '+rightWristX+' rightWristY = '+rightWristY);
+} 
+}  
+   
 
 function draw(){     
 image(video, 0, 0, 600, 500); 
